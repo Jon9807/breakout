f2c936a780880c5663f383b5c40176c1e4f394c2
Ngun Hmun <you@example.com>
2024-05-31 01:08:40 -0400

    touch control

--- paddle.js
+++ paddle.js
@@ -1,5 +1,3 @@
-//paddle.js
-
 export class Paddle {
     constructor(canvas, width, height, x) {
         this.canvas = canvas;
@@ -12,6 +10,7 @@ export class Paddle {
 
         document.addEventListener('keydown', this.keyDownHandler.bind(this), false);
         document.addEventListener('keyup', this.keyUpHandler.bind(this), false);
+        this.addTouchControls();
     }
 
     draw() {
@@ -44,6 +43,32 @@ export class Paddle {
         } else if (e.key === "Left" || e.key === "ArrowLeft") {
             this.leftPressed = false;
         }
+    }
+
+    addTouchControls() {
+        let touchStartX = null;
+        let touchEndX = null;
+
+        this.canvas.addEventListener('touchstart', (e) => {
+            touchStartX = e.changedTouches[0].clientX;
+        });
+
+        this.canvas.addEventListener('touchmove', (e) => {
+            touchEndX = e.changedTouches[0].clientX;
+            let deltaX = touchEndX - touchStartX;
+            if (deltaX > 30) {
+                this.rightPressed = true;
+                this.leftPressed = false;
+            } else if (deltaX < -30) {
+                this.leftPressed = true;
+                this.rightPressed = false;
+            }
+        });
+
+        this.canvas.addEventListener('touchend', () => {
+            this.rightPressed = false;
+            this.leftPressed = false;
+        });
     }
 
     reset(x) {
